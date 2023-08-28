ecff413234fe1ef4e1aedc151e86a7c0344c0327
fzhao888 <frank.zhao93@gmail.com>
2023-08-28 15:49:23 -0500

    Added controllers

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -3,12 +3,28 @@ const updatePostHandler = async(event) => {
     const content = document.querySelector('update-content').value.trim();
 
     if(title && content){
-        
+        const response = await fetch('/api/blogposts/:id', {
+            method: 'PUT',
+            body: JSON.stringify({title, content}),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        });
+    }
+
+    if(response.ok){
+        document.location.replace('/blogPost/:id');
     }
 }
 
 const deletePostHandler = async(event) => {
+    const reponse = await fetch('api/blogposts/:id', {
+        method: 'DELETE' 
+    })
 
+    if(response.ok){
+        document.location.replace('/profile');
+    }
 }
 
 document    
